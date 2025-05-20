71d0dc7de0a2452a5b3321d49787d7fe6a1cd9b0
“nutrilorenabarbosa” <havaizinhogourmet@gmail.com>
2025-05-19 23:17:07 -0300

    atividade aula ia

---
+++ Pratica01.py
@@ -0,0 +1,15 @@
+print ("olá mundo")
+A = 10
+B = 20
+C = A + B
+print("o resultado da soma é:", C)
+Cp = 10
+La = 20
+Al = 30
+Vl = Cp * La * Al 
+print("o volume é:", Vl,"cm3")
+Produto = "repelente"
+Preco_unitario = 15
+Quantidade = 20
+Preco_total = Preco_unitario * Quantidade
+print("o nome do Produto é:", Produto, "o Preco_unitario é:", Preco_unitario, "a Quantidade é:", Quantidade, "e o Preco_total é", Preco_total)
